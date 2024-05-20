54d47c85bb08ca9d4ecce83abc316d4dd5ff9b8e
kerokero014 <kevinjrm.kr@gmail.com>
2024-05-20 15:34:36 -0600

    starting Spring Boot Project
    devtools, mongodb, and webdevtools for springboot JAVA

---
+++ FlashyStudy/src/main/java/com/example/FlashyStudy/FlashyStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.FlashyStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashyStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashyStudyApplication.class, args);
+	}
+
+}
