ecb6f775ea69934ebbd6021ffa1040778dbd54df
kerokero014 <kevinjrm.kr@gmail.com>
2024-05-20 17:22:26 -0600

    deleted
    un-used imports

--- FlashyStudy/src/main/java/com/example/FlashyStudy/FlashyStudyApplication.java
+++ FlashyStudy/src/main/java/com/example/FlashyStudy/FlashyStudyApplication.java
@@ -2,8 +2,7 @@ package com.example.FlashyStudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class FlashyStudyApplication {
